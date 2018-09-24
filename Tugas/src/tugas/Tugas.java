9e4d19d0ff2de4b31f9d929631f42c0f1fc01fc7
MUbaidillah <43540601+MUbaidillah@users.noreply.github.com>
2018-09-24 19:24:24 +0700

    Add files via upload

---
+++ Tugas/src/tugas/Tugas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fajar
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Muhammad Ubaidillah");
+        int umur = 27;
+        boolean cantik = false;
+        if(umur > 23 && cantik == true ){
+            System.out.println("Nikah yuk");
+    }
+        else if (umur > 23){
+            System.out.println("saya pikir dulu ya");
+    }
+        else
+            System.out.println("Maaf ya");
+    }
+}
